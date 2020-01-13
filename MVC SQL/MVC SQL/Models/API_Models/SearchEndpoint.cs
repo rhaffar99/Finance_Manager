b26e51d6a43f36385d38e27a89e9acbca1c8d61e
Raymond Haffar <r2999haf@edu.uwaterloo.ca>
2020-01-13 16:09:38 -0500

    Added API object model for basic stock search

---
+++ MVC SQL/MVC SQL/Models/API_Models/SearchEndpoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_SQL.Models.API_Models
+{
+    public class SearchEndpoint
+    {
+        string symbol { get; set; }
+        double open { get; set; }
+        double high { get; set; }
+        double low { get; set; }
+        double price { get; set; }
+        int volume { get; set; }
+        string latest_trading_day { get; set; }
+        double previous_close { get; set; }
+        double change { get; set; }
+        string change_percent { get; set; }
+    }
+}
