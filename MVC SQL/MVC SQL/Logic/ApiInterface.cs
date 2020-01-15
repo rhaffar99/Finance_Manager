a16f9d6ccaa120a8726f8abd68108ade3c36b742
Raymond Haffar <r2999haf@edu.uwaterloo.ca>
2020-01-15 16:37:05 -0500

    Added form for user input

---
+++ MVC SQL/MVC SQL/Logic/ApiInterface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_SQL.Logic
+{
+    public static class ApiInterface
+    {
+        const string apiKey = "ECQW7XET4W7RC9TT";
+        static public string generateSearchUrl(string symbol)
+        {
+            string baseUrl = "https://www.alphavantage.co/query?";
+            baseUrl += "function=GLOBAL_QUOTE";
+            baseUrl += $"&symbol={symbol}";
+            baseUrl += $"&apikey={apiKey}";
+
+            return baseUrl;
+        }
+    }
+}
