4ee9a490906cc47344060c3641c16416a682ce2b
Raymond Haffar <r2999haf@edu.uwaterloo.ca>
2020-02-24 16:31:23 -0500

    Added empty models for collections endpoints with multiple datapoints over period of days -> months

---
+++ MVC SQL/MVC SQL/Models/API_Models/TimeSeriesDailyModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_SQL.Models.API_Models
+{
+    public class TimeSeriesDailyModel
+    {
+
+    }
+
+    public class DailyQuoteModel
+    {
+
+    }
+}
